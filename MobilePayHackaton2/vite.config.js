9f3028c44c365dce6aee222b593e74b5bcdfa37a
Tautvydas <tautvydas.kuk@gmail.com>
2023-03-23 18:24:40 +0200

    fixed project

--- MobilePayHackaton2/vite.config.js
+++ MobilePayHackaton2/vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
+        laravel({                
+            input: [            
+                'resources/css/app.css',
+                'resources/js/app.jsx'
+            ],
             refresh: true,
+            postcss: {
+                plugins: [
+                    autoprefixer(),
+                ]
+            },
         }),
         react(),
     ],
+
+    server: {
+        watch: {
+          usePolling: true
+        }
+    }
 });
+
